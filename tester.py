ce5f7e322586ead6b2e58287ad0abd934f8d1b80
pete1328 <agirlpete01@gmail.com>
2024-04-04 11:57:58 -0400

    adjusting for pylint

--- tester.py
+++ tester.py
@@ -1,5 +1,5 @@
 import asyncio
-from distutils.cmd import Command # pip install -U aiogram
+#from distutils.cmd import Command # pip install -U aiogram
 import logging
 import sys
 from os import getenv
@@ -9,14 +9,14 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.filters.command import CommandObject
 from aiogram.types import Message
-from aiogram.utils.markdown import hbold, bold, text #hspoiler
+from aiogram.utils.markdown import hbold #bold, text #hspoiler
 from telegram.ext import ConversationHandler
 
 # Bot token can be obtained via https://t.me/BotFather
 #TOKEN = str(getenv("BOT_TOKEN")) #11/14 not working
 
 API_TOKEN = '6789765586:AAE-XUmzOAfF_X6fVrNiFCn7Wln4yD5jWcs'
-user_key = 'BPS1600xyz' # what users should send bot when prompted, to get access 
+USER_KEY = 'BPS1600xyz' # what users should send bot when prompted, to get access
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
@@ -39,16 +39,14 @@ async def command_start_handler(message: Message) -> None:
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     #GIVEN: await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
-    await message.reply(F"Welcome {hbold(message.from_user.full_name)}! To continue, please type /signup")
+    await message.reply(F"Welcome {hbold(message.from_user.full_name)}! Now please /signup")
 
 
 # Custom Commands...
-""" /signup - how a new user proves they should be on the research team list
-    /checkstatus - to get an immediate update on the main fridge stats
-    /errorhistory - to get list of past fridge issues and dates"""
 
 @dp.message(Command("signup"))
 async def command_signup_handler(message: Message) -> None:
+    """ /signup - how a new user proves they should be on the research team list"""
     # ask user to send the LHQS Bot access code.
     await message.answer("Let's get you signed up! Please respond with the LHQS code to qualify.")
     # if proper code, add user id to user_list
@@ -71,10 +69,12 @@ async def command_signup_handler(message: Message) -> None:
 
 @dp.message(Command("checkstatus"))
 async def command_status_handler(message: Message) -> None:
+    """/checkstatus - to get an immediate update on the main fridge stats"""
     await message.answer("How's our fridge doin you ask?")
 
 @dp.message(Command("errorhistory"))
 async def command_errors_handler(message: Message) -> None:
+    """/errorhistory - to get list of past fridge issues and dates"""
     await message.answer("Below is the error history for FRIDGEX")
 
 #@dp.
